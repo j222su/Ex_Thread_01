f39b50ed5228c878447f2d37bb82b50d75803930
jisu <patrickstar0910@gmail.com>
2021-09-23 17:50:53 +0900

    ADD : using class

---
+++ src/main/java/com/js/example_thread/MyThread.java
@@ -0,0 +1,8 @@
+package com.js.example_thread;
+
+public class MyThread extends Thread{
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+    }
+}
