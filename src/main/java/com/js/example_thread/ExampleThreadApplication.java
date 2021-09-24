3c6069d69e0a812a3f1b16bd12230b6d63be8c0c
jisu <patrickstar0910@gmail.com>
2021-09-24 14:21:01 +0900

    IMP : using Thread class

--- src/main/java/com/js/example_thread/ExampleThreadApplication.java
+++ src/main/java/com/js/example_thread/ExampleThreadApplication.java
@@ -10,32 +10,10 @@ public class ExampleThreadApplication {
 		SpringApplication.run(ExampleThreadApplication.class, args);
 
 		MyThread myThread01 = new MyThread();
-		MyThread myThread02 = new MyThread();
-		MyThread myThread03 = new MyThread();
-
-		Runnable myRunnable01 = new MyRunnable();
-		Runnable myRunnable02 = new MyRunnable();
-		Runnable myRunnable03 = new MyRunnable();
-
-		Thread thread01ForRunnable = new Thread(myRunnable01);
-		Thread thread02ForRunnable = new Thread(myRunnable02);
-		Thread thread03ForRunnable = new Thread(myRunnable03);
 
 		myThread01.setName("myThread01");
-		myThread02.setName("myThread02");
-		myThread03.setName("myThread03");
-
-		thread01ForRunnable.setName("myRunnable01");
-		thread02ForRunnable.setName("myRunnable02");
-		thread03ForRunnable.setName("myRunnable03");
-
 
 		myThread01.start();
-		thread01ForRunnable.start();
-		myThread02.start();
-		myThread03.start();
-		thread02ForRunnable.start();
-		thread03ForRunnable.start();
 
 	}
 
